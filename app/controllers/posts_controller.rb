a097f5611cf2c8dad665731c21f8af9da4e9ae5d
panyujin <panyujinyx@gmail.com>
2017-03-24 14:32:48 +0800

    implement my groups

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,6 +7,11 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
+  def edit
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+  end
+
   def create
     @group =Group.find(params[:group_id])
     @post = Post.new(post_params)
@@ -20,11 +25,28 @@ class PostsController < ApplicationController
     end
   end
 
+  def update
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+
+    if @post.update(post_params)
+      redirect_to account_posts_path, notice: "Update Success"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to account_posts_path, alert: "Post deleted"
+  end
+
 
   private
 
   def post_params
     params.require(:post).permit(:content)
   end
-  
+
 end
